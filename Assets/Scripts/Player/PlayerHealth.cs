3a2d543fa3ea947c91c5fcf130f774124c3c2311
agRivera1869 <146147060+agRivera1869@users.noreply.github.com>
2024-03-25 18:32:29 -0700

    Update PlayerHealth.cs

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -5,19 +5,20 @@ using Unity.Netcode;
 
 public class PlayerHealth : NetworkBehaviour
 {
-    public int maxHealth = 10;
-    public int health;
+    public int maxHealth = 10;  //initialize the max health a player can be at
+    public int health;          //variable to track current health
     void Start()
     {
-        health = maxHealth;
+        health = maxHealth;     //set health to full
     }
 
-    public void TakeDamage(int damage)
-    {
-        health -= damage;
-        if (health <= 0)
+    public void TakeDamage(int damage)  //method for when a player takes damage
+    {  
+        health -= damage;      //decrease player health by enemy's damage
+        if (health <= 0)       //if player health reaches 0
         {
-            transform.position = new Vector3(0, -100, 0);
+            transform.position = new Vector3(0, -100, 0);   //transform away from scene
+            //DestroyImmediate(gameObject, true);           //[TRY TO FIX] destroy game object
         }
     }
 }
